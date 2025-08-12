210ba0bd0ff4cd891e382d624515545c97178bdb
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-08-12 12:57:48 +0530

    Create Evaluate_Formula.py

---
+++ PYTHON_BASICS/Evaluate_Formula.py
@@ -0,0 +1,9 @@
+class Solution:
+    def calculate(self, a: int, b: int, c: int, d: int) -> int:
+        #Code here
+   return (a + b) / c + d
+   
+   s = Solution()
+   print(s.calculate())
+  
+  

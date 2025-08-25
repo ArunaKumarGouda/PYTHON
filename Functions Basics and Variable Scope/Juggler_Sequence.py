9b460c408e5be11dcd7479e907367fd098f98dd7
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-08-25 22:17:50 +0530

    Create Juggler_Sequence.py

---
+++ Functions Basics and Variable Scope/Juggler_Sequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def jugglerSequence(self, n):
+        # code here
+        sequence = [n]
+        while n != 1:
+            if n % 2 == 0:
+                n = int(n ** 0.5)
+            else:
+                n = int(n ** 1.5)
+            sequence.append(n)
+        return sequence

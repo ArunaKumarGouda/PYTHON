39851767bd3952e00db891aed253d2fa29e40bec
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-02-11 11:50:48 +0530

    Add type checking examples in 02_type.py

---
+++ Python/02_type.py
@@ -0,0 +1,20 @@
+a = 4
+b = type(a)
+print(b)
+
+c = 5.4
+d = type(c)
+print(d)
+
+e = "Aruna"
+f = type(e)
+print(f)
+
+g = True
+h = type(g)
+print(h)
+
+i = "45.8"
+j = float(i)
+k = type(j)
+print(k)

436323a29e88756c9856af82032f7fa19e45794d
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-02-11 11:50:06 +0530

    Add simple calculator with arithmetic and logical operations
    Implemented a simple calculator with basic arithmetic and logical operations.

---
+++ Python/a_01_operators.py
@@ -0,0 +1,23 @@
+# To make a simple calculator in python.
+
+x = 20000
+y = 90000
+
+a = x + y
+print("add", a)
+
+b = x - y
+print("sub", b)
+
+c = x * y
+print("mul", c)
+
+d = x / y
+print("div", d)
+
+name = "Aruna"
+print(name)
+
+#logical operator
+z = True and False
+print(z)

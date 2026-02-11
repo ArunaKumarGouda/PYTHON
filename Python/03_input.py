79d18f72f5a12f61d2c693b537854807e9b3af1e
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-02-11 11:52:03 +0530

    Add input handling for strings and integers

---
+++ Python/03_input.py
@@ -0,0 +1,15 @@
+#Input String
+a = input("Enter number 1 : ")
+b = input("Enter number 2 : ")
+
+print("The number is : ", a)
+print("The numbeer is : ", b)
+print("The sum is :", a + b)
+
+#input Integer
+c = int(input("Enter number 1 : "))
+d = int(input("Enter number 2 : "))
+
+print("The number is : ", c)
+print("The numbeer is : ", d)
+print("The sum is :", c + d) 

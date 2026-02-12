434f524e785518212e8fa6162fe8d706b9797e94
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-02-12 21:58:33 +0530

    Add multiple Python problems for practice

---
+++ Python/04_problems1.py
@@ -0,0 +1,38 @@
+# Problem 1 :
+# Write a python program to add to numbers
+a = 45
+b = 4
+print("a = ", a)
+print("b = ", b)
+print("The sum of a and b is : ", a + b)
+
+
+# Problem 2 :
+# Write a python program to find remainder when a number is divided by 2.
+c = 45
+d = 2
+print("Reminder when a is divided by b is :", a % b)
+
+
+# Problem 3 :
+# Check the type of variable assigned by input () function.\
+e = input("Enter the value of a :")
+print(type(a))
+
+# Propblem 4 : 
+# Use comparison operator to find out whether a given variable f is greater then g or not. Take f = 30 or g = 80. 
+f = 30
+g = 80
+print(f > g)
+
+# Problem 5 :
+# Write a python program to find an average of two numbers entered by the user.
+h = float(input("Enter number 1 :"))
+i = float(input("Enter number 2 :"))
+print("The average of two number is :", (h + i) / 2)
+
+
+# problem 6:
+# Write a python program to calculate the square of a number entered by the user
+j = int(input("Enter number 1 :"))
+print("The square of two number is :", j ** 2)

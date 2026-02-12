af6549d50237b09c9f390c00bfb7e0fc2552f0bb
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-02-13 00:10:25 +0530

    Add string slicing examples in 05_string.py

---
+++ Python/05_string.py
@@ -0,0 +1,8 @@
+name = "Aruno0oiop9ia"
+nameshort = name[0:3] # 0 to length -1
+print(nameshort)
+print(name[-4:-1]) # Output is run
+print(name[1:4])
+
+number = "0123456789"
+print(number[1 : 8 : 3]) # output is : 147

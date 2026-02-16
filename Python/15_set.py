769fa2ddc7491d7b06a117983d3c843936b76f44
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-02-17 00:26:59 +0530

    Create 15_set.py

---
+++ Python/15_set.py
@@ -0,0 +1,18 @@
+d = {}  # This is an empty dictionary
+print(type(d))
+
+s = set()  # This is an empty set
+print(type(s))
+
+# How to write set
+s = {3, 345, "Aruna"}
+print(type(s))
+print(s)
+
+# This is the difference between set and dictionary
+d = {
+    "Aruna": 58, 
+    "Basanta": 34
+}
+print(type(d))
+print(d)

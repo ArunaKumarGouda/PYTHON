1ada1bf6d27f2d4580858a1f0f3c91b61d7683ab
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-02-22 09:20:50 +0530

    Create 25_for_loops.py with for loop examples
    Add examples of for loops with different parameters.

---
+++ Python/25_for_loops.py
@@ -0,0 +1,13 @@
+# for(start, stop, step-size)
+
+print("Print only for loop")
+for i in range(9):
+    print(i)
+
+print("Print using start and stop for loop")
+for i in range(1, 11):
+    print(i)
+
+print("Print using start, stop and step-size for loop")
+for i in range(1, 30, 4):
+    print(i)

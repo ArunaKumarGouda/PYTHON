62d0f0d918183d1cc1dff27eeb7c48f29dc578f5
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-02-23 09:33:30 +0530

    Add for loop examples for list, tuple, and string
    Demonstrate iterating over a list, tuple, and string using for loops.

---
+++ Python/26_for_loop_iterate.py
@@ -0,0 +1,18 @@
+# Print the list using for loop
+print("Print the list using for loop")
+l = ["Aruna", "Ritwick", 38, "Basanta", 59.0]
+
+for i in l:
+    print(i)
+
+#Print the tuple using for loop
+print("Print the tuple using for loop")
+t = ("Aruna", 4739, 3487.589, "Abinash")
+
+for i in t:
+    print(i)
+
+# Printing all characters in a string using for loop
+s = "Aruna"
+for i in s:
+    print(i)

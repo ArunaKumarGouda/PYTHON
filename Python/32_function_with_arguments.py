64d25736fd734ef956f005575b1c41d8e6547134
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-02-26 00:14:54 +0530

    Add function to greet with a name and ending

---
+++ Python/32_function_with_arguments.py
@@ -0,0 +1,10 @@
+def goodDay(name, ending):
+    print("Good Day,", name)
+    print(ending)
+
+    return("Have a nice day")
+
+goodDay("Aruna", "Thank You.")
+
+a = goodDay("Ritwick", "Thanks.")
+print(a)

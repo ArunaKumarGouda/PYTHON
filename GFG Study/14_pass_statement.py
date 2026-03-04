684b13c0c26f064b26289574201bf442c7a3d9fd
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-03-04 09:30:34 +0530

    Add example usage of 'pass' statement in Python
    Demonstrate the use of 'pass' statement in functions, conditional statements, loops, and classes.

---
+++ GFG Study/14_pass_statement.py
@@ -0,0 +1,38 @@
+def fun():
+    pass
+
+fun()
+
+
+# In Conditional Statements
+x = 10
+
+if x > 5:
+    pass  # Placeholder for future logic
+else:
+    print("x is 5 or less")
+
+
+# In Loops
+for i in range(5):
+    if i == 3: 
+        pass    # Do nothing while i is 3
+    else:
+        print(i)
+
+
+
+# In Classes
+class EmptyClass:
+    pass  # No methods or attributes yet
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def greet(self):
+        pass  # Placeholder for greet method
+
+# Creating an instance of the class
+p = Person("Emily", 30)

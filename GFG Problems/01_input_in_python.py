9070fe25a68558f0cd14d2d76a228db0d46bb329
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-03-09 00:32:51 +0530

    Uncomment input and print statements in Python script

---
+++ GFG Problems/01_input_in_python.py
@@ -0,0 +1,19 @@
+# s = input()
+# print(s)
+
+# i = int(input())
+# n = i + 10
+# print(n)
+
+# p = float(input())
+# f = p * 10
+# print(f)
+
+s = input()
+print(s)
+
+n = int(input())
+print(n + 10)
+
+f = float(input())
+print(f * 10)

3b243f5012184572a99d4395ce3cf70a67800bf8
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-03-15 20:30:03 +0530

    Create 50_multilevel_inheritance.py

---
+++ Python/50_multilevel_inheritance.py
@@ -0,0 +1,20 @@
+# When a child class becomes a parent for another child class is called multilevel iniheritance.
+
+class Employee:
+    a = 1
+
+class Programmer(Employee):
+    b = 2
+
+class Manager(Programmer):
+    c = 3
+
+o = Employee()
+print(o.a)  # Prints the a attribute
+# print(o.b)  # Shows an error as there is no attribute in Employee class
+
+o = Programmer()
+print(o.a, o.b)
+
+o = Manager()
+print(o.a, o.b, o.c)

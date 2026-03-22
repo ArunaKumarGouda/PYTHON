4b5211f94112d1889fb88dde9b0e72aebf582723
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-03-22 10:43:43 +0530

    Add helloFunction that prints 'hello'

---
+++ GFG Problems/14_function_with_no_argument.py
@@ -0,0 +1,8 @@
+# Use function template for python3
+
+# Write the helloFunction below. It should have no statement
+# print("hello")
+# Code here
+def helloFunction():
+    print("hello")
+helloFunction()

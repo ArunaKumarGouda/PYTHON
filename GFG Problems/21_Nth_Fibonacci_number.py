7640c2bc99476698aeb1d7b36802ab6e2e4ce07d
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-03-26 19:48:45 +0530

    Create 21_Nth_Fibonacci_number.py

---
+++ GFG Problems/21_Nth_Fibonacci_number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nthFibonacci(self, n: int) -> int:
+        if n <= 1:
+            return n
+        
+        a, b = 0, 1
+
+        for i in range(2, n + 1):
+            a, b = b, a + b
+        return b
+
+object = Solution()
+result = object.nthFibonacci(6)
+print(result)

6ee2858c323d535d1cd306254a0b394df1f1caa8
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-04-15 23:16:26 +0530

    Add string formatting example in 02_string_formating.py

---
+++ Eduskill_Internship/02_string_formating.py
@@ -0,0 +1,6 @@
+# Write your code here
+# Example:
+product_name = "Keyboard"
+product_price = 75.99
+is_available = True
+print(f"Product: {product_name}, Price: ${product_price:.2f}, Available: {'Yes' if is_available else 'No'}")

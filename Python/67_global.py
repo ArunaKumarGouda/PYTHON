4ccc5ebd94197d855a64d505d4d487d5fa9cfccf
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-05-04 18:43:59 +0530

    Add global variable and modify in function
    Define a global variable and modify it within a function.

---
+++ Python/67_global.py
@@ -0,0 +1,15 @@
+a = 43  # Global value
+def fun():
+    global a    # Change the globaa = 43  # Global value
+def fun():
+    global a    # Change the global value to local value
+    a = 8
+    print(a)
+
+fun()
+print(a)l value to local value
+    a = 8
+    print(a)
+
+fun()
+print(a)
